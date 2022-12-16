a94a423a2ffc182355fcb382fea341e51e0d4433
shiiiiyd <sydbgx@gmail.com>
2022-12-16 20:41:39 +0800

    fix: 设置swag api，执行swag init未成功
    执行swag init 出现如下错误
    ParseComment error in file /Users/shiyd/go/src/github.com/shiiiiyd/go_gateway/controller/service.go :example value  can't convert to integer err: strconv.Atoi: parsing "": invalid syntax

--- controller/service.go
+++ controller/service.go
@@ -13,6 +13,16 @@ func ServiceRegister(group *gin.RouterGroup) {
 	group.GET("/service_list", service.ServiceList)
 }
 
+// ServiceList godoc
+// @Summary 服务列表
+// @Description 服务列表
+// @Tags 服务管理
+// @ID /service/service_list
+// @Accept  json
+// @Produce  json
+// @Param polygon body dto.ServiceListInput true "body"
+// @Success 200 {object} middleware.Response{data=dto.ServiceListOutput} "success"
+// @Router /service/service_list [get]
 func (service ServiceController) ServiceList(ctx *gin.Context) {
 	params := &dto.ServiceListInput{}
 	err := params.BindValidParam(ctx)
