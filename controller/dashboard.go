94da593f299715abe445c94e3567e39cc5372826
shiiiiyd <sydbgx@gmail.com>
2022-12-29 22:53:36 +0800

    feat: 首页大盘，服务统计

--- controller/dashboard.go
+++ controller/dashboard.go
@@ -3,27 +3,32 @@ package controller
 import (
 	"github.com/e421083458/golang_common/lib"
 	"github.com/gin-gonic/gin"
+	"github.com/pkg/errors"
 	"go_gateway/dao"
 	"go_gateway/dto"
 	"go_gateway/middleware"
+	"go_gateway/public"
+	"time"
 )
 
 type DashboardController struct{}
 
 func DashboardRegister(group *gin.RouterGroup) {
 	service := &DashboardController{}
-	group.GET("/panelGroupData", service.PanelGroupData)
+	group.GET("/panel_group_data", service.PanelGroupData)
+	group.GET("/flow_stat", service.FlowStat)
+	group.GET("/service_stat", service.ServiceStat)
 }
 
 // PanelGroupData godoc
 // @Summary 指标统计
 // @Description 指标统计
 // @Tags 首页大盘
-// @ID /dashboard/panelGroupData
+// @ID /dashboard/panel_group_data
 // @Accept  json
 // @Produce  json
 // @Success 200 {object} middleware.Response{data=dto.PanelGroupDataOutput} "success"
-// @Router /dashboard/panelGroupData [get]
+// @Router /dashboard/panel_group_data [get]
 func (service *DashboardController) PanelGroupData(c *gin.Context) {
 	tx, err := lib.GetGormPool("default")
 	if err != nil {
@@ -50,6 +55,71 @@ func (service *DashboardController) PanelGroupData(c *gin.Context) {
 		AppNum:          appNum,
 		CurrentQPS:      0,
 		TodayRequestNum: 0,
+	}
+	middleware.ResponseSuccess(c, out)
+}
+
+// FlowStat godoc
+// @Summary 服务统计
+// @Description 服务统计
+// @Tags 首页大盘
+// @ID /dashboard/flow_stat
+// @Accept  json
+// @Produce  json
+// @Param id query string true "服务ID"
+// @Success 200 {object} middleware.Response{data=dao.ServiceStatOutput} "success"
+// @Router /dashboard/flow_stat [get]
+func (service *DashboardController) FlowStat(c *gin.Context) {
+	var todayList []int64
+	for i := 0; i <= time.Now().Hour(); i++ {
+		todayList = append(todayList, 0)
+	}
+	var yesterdayList []int64
+	for i := 0; i < 23; i++ {
+		yesterdayList = append(yesterdayList, 0)
+	}
+	middleware.ResponseSuccess(c, &dto.ServiceStatOutput{
+		Today:     todayList,
+		Yesterday: yesterdayList,
+	})
+}
+
+// ServiceStat godoc
+// @Summary 服务统计
+// @Description 服务统计
+// @Tags 首页大盘
+// @ID /dashboard/service_stat
+// @Accept  json
+// @Produce  json
+// @Success 200 {object} middleware.Response{data=dto.DashServiceStatOutput} "success"
+// @Router /dashboard/service_stat [get]
+func (service *DashboardController) ServiceStat(c *gin.Context) {
+	tx, err := lib.GetGormPool("default")
+	if err != nil {
+		middleware.ResponseError(c, 2000, err)
+		return
+	}
+
+	serviceInfo := &dao.ServiceInfo{}
+	list, err := serviceInfo.GroupByLoadType(c, tx)
+	if err != nil {
+		middleware.ResponseError(c, 2001, err)
+		return
+	}
+
+	var legend []string
+	for index, item := range list {
+		name, ok := public.LoadTypeMap[item.LoadType]
+		if !ok {
+			middleware.ResponseError(c, 2002, errors.New("load_type not found"))
+			return
+		}
+		list[index].Name = name
+		legend = append(legend, name)
+	}
+	out := &dto.DashServiceStatOutput{
+		Legend: legend,
+		Data:   list,
 	}
 	middleware.ResponseSuccess(c, out)
 }
